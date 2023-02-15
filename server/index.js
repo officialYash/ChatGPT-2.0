a773b909d831ca61a75b86dc9f562fb212cb647a
officialYash <38553961+officialYash@users.noreply.github.com>
2023-02-15 21:09:08 +0530

    Set API Configuration in the server File

--- server/index.js
+++ server/index.js
@@ -8,7 +8,36 @@ const app = express();
 env.config();
 app.use(cors());
 app.use(bodyParser.json());
+ 
+// Configure open API 
+const configuration = new Configuration({
+    organization:"org-2AefQvyFqFCBsN9GrbKctS9Y",
+    apiKey: process.env.API_KEY, 
+})
 
+const openai = new OpenAIApi(configuration) 
+// Listening
 app.listen("3080" ,()=>console.log("listening on port 3080"))
 
+// Dummy Router to router
 app.get("/" , (req ,res ) => {res.send("Hello World!")});
+
+// post route for making requests 
+
+app.post('/', async (req ,res) =>{
+    const {message} = req.body
+    try{
+        const response = await openai.createCompletion({
+            model : "text-devinci-003",
+            prompt: `${message}`,
+            max_tokens:100, 
+            temperature: .5
+        })
+
+        res.json({message:response.data.choices[0].value})
+    }
+    catch(e){
+console.log(e)
+res.send(e).status(400)
+    }
+})
