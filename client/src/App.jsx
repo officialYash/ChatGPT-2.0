5d0c6555513f177146ed11e7c7b677579e0b0f59
officialYash <38553961+officialYash@users.noreply.github.com>
2023-02-15 20:08:50 +0530

    Completed a part of fronted End Side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,24 @@
-import './App.css'
 import ChatBody from './components/ChatBody'
+import ChatInput from './components/ChatInput'
 
 function App() {
   return (
-<div className='bg-[#1A232E] h-screen py-6 relative sm:px-28 text-white overflow-hidden flex flex-col justify-between align-middle'>
+<div className='bg-[#1A232E] h-screen py-6 relative sm:px-16 px-12 text-white overflow-hidden flex flex-col justify-between align-middle'>
 
 <div className=' gradient-01 z-0 absolute'></div>
 <div className=' gradient-02 z-0 absolute'></div>
 
   {/* Header */}
-  <div className='uppercase font-bold text-2xl text-center'>
+  <div className='uppercase font-bold text-2xl text-center mb-3 '>
 ChatGPT 2.0
   </div>
 
   {/* body */}
 
-  <div> <ChatBody/> </div>
-
+  <div className="h-[90%] overflow-auto w-full max-w-4xl min-w-[20rem] py-8 px-4 self-center"> <ChatBody/> </div>
   {/* input */}
 
-  <div>input </div>
+  <div className='w-full max-w-4xl min-w-[20rem] self-center'><ChatInput/> </div>
 </div>
   )
 }
--- client/src/components/ChatBody.jsx
+++ client/src/components/ChatBody.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const ChatBody = () => {
-    const aiStyle =" bg-white bg-opacity-40 backdrop-blur-lg dropshadow-md"
+    const aiStyle = 'bg-white bg-opacity-40 backdrop-blur-lg dropshadow-md mr-auto' 
   return (
-    <div className='flex flex-col gap-4'> 
+    <div className='flex flex-col gap-4 '> 
     
     {/* client Message */}
     <div className='border-[#999999] break-words border-2 rounded-xl self-end  px-3 py-3  max-w-[80%]'>
-    <pre className=' whitespace-pre-wrap'>
+    <pre className='whitespace-pre-wrap'>
       <span>
         Hy Chat GPT , Can you Help me ?
       </span>  
@@ -16,15 +16,14 @@ const ChatBody = () => {
     </div>
 {/* ai Message */}
 
-<div className={`border-[#999999] break-words border-2 rounded-xl self-end  px-3 py-3  max-w-[80%]'>
-    <pre className=' whitespace-pre-wrap' ${aiStyle}>
+<div className={`border-[#999999] break-words border-2 rounded-xl self-end px-3 py-3 max-w-[80%]'>
+    <pre className=' whitespace-pre-wrap ${aiStyle} mr-auto>
       `}>
     <pre>
       <span>
         Yeah , I can help you with anything 
       </span>  
     </pre>
-
     </div>
 
     </div>
