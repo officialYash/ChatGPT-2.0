17328bace6c2c9200c1aa64f12b7c7bb489b17f6
officialYash <38553961+officialYash@users.noreply.github.com>
2023-02-15 18:01:42 +0530

    Set a home page of ChatGPT 2.0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+<div className='bg-[#1A232E] h-screen py-6 relative sm:px-28 text-white overflow-hidden flex flex-col justify-between align-middle'>
+
+<div className=' gradient-01 z-0 absolute'></div>
+<div className=' gradient-02 z-0 absolute'></div>
+
+  {/* Header */}
+  <div className='uppercase font-bold text-2xl text-center'>
+ChatGPT 2.0
+  </div>
+
+  {/* body */}
+
+  <div> body</div>
+
+  {/* input */}
+
+  <div>input </div>
+</div>
   )
 }
 
