b4f281d5882ba4d3bc0187e6bd4116a433a02ece
officialYash <38553961+officialYash@users.noreply.github.com>
2023-02-16 17:17:29 +0530

    all File Are set for a ready ChatGPT 2.0

--- client/src/api.js
+++ client/src/api.js
@@ -1,18 +1,20 @@
-export const fetchResponse = async(chat) =>{
-    try{
-const response = await fetch('http://localhost:3080/',{
-    method:'POST',
-    headers:{
-        "Content-type":"application/json"
-    },
-    body:JSON.stringify({
-        message: chat.map((message)=> message.message).join("\n")
-    })
-})
+export const fetchResponse =  async(chat) => {
+    try {
+        // after depoloyment you should change the fetch URL below
+        const response = await fetch('http://localhost:3080', { 
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                message: chat.map((message)=> message.message).join(" \n ")
+            })
+        })
 
-const data = await response.json();
-}
-catch (error) {
-console.log(error);
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.log(error);
     }
 }
+Footer
--- server/index.js
+++ server/index.js
@@ -1,43 +1,51 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import env from "dotenv";
-import {Configuration, OpenAIApi } from "openai";
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import env from 'dotenv'
+import {Configuration, OpenAIApi} from 'openai'
 
-const app = express();
-env.config();
-app.use(cors());
-app.use(bodyParser.json());
- 
-// Configure open API 
+const app = express()
+
+env.config()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
+// Configure open api
 const configuration = new Configuration({
     organization:"org-2AefQvyFqFCBsN9GrbKctS9Y",
-    apiKey: process.env.API_KEY, 
+    apiKey: process.env.API_KEY 
 })
 
-const openai = new OpenAIApi(configuration) 
-// Listening
-app.listen("3080" ,()=>console.log("listening on port 3080"))
+const openai = new OpenAIApi(configuration)
 
-// Dummy Router to router
-app.get("/" , (req ,res ) => {res.send("Hello World!")});
 
-// post route for making requests 
+// listeninng
+app.listen("3080", ()=>console.log("listening on port 3080"))
 
-app.post('/', async (req ,res) =>{
+
+// dummy route to test
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+
+//post route for making requests
+app.post('/', async (req, res)=>{
     const {message} = req.body
+
     try{
         const response = await openai.createCompletion({
-            model : "text-devinci-003",
+            model: "text-davinci-003",
             prompt: `${message}`,
-            max_tokens:100, 
+            max_tokens: 100,
             temperature: .5
         })
+        res.json({message: response.data.choices[0].text})
 
-        res.json({message:response.data.choices[0].value})
-    }
-    catch(e){
-console.log(e)
-res.send(e).status(400)
+    }catch(e){
+        console.log(e)
+        res.send(e).status(400)
     }
 })
